ae073a4f85001033f2ca75a8bba47e26e330f38f
Bilsyp <bilalsyah77@gmail.com>
2022-06-29 17:09:07 +0700

    fix background

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
 const user = document.querySelector(".logo");
 const menu = document.querySelector(".open");
 const aside = document.querySelector("#aside");
-const closeAside = document.querySelector("#close-aside")
-closeAside.addEventListener("click",()=>{
-aside.setAttribute("data-visible","false")
-})
-user.addEventListener("click",()=>{
+const navigation = document.querySelector(".navigation");
+const closeAside = document.querySelector("#close-aside");
+closeAside.addEventListener("click", () => {
+  aside.setAttribute("data-visible", "false");
+});
+user.addEventListener("click", () => {
+  aside.style.animationPlayState = "running";
+  const attr = aside.getAttribute("data-visible");
+  if (attr == "false") {
+    aside.setAttribute("data-visible", "true");
+  } else {
+    aside.setAttribute("data-visible", "false");
+  }
+});
 
-aside.style.animationPlayState="running"
-const attr = aside.getAttribute("data-visible")
-if(attr=="false"){
-aside.setAttribute("data-visible","true");
-}else{
-aside.setAttribute("data-visible","false");
-}
-})
+menu.addEventListener("click", () => {
+  const attrs = navigation.getAttribute("data-visible");
+  if (attrs == "false") {
+    navigation.setAttribute("data-visible", "true");
+    menu.innerHTML = `<i class="fas fa-times fa-2x"></i>`;
+  } else {
+    navigation.setAttribute("data-visible", "false");
+    menu.innerHTML = `<i class="fas fa-bars fa-2x"></i>`;
+  }
+});
